305f8eeb94ea0b7d9bd5a3f2877243ea9d5c5bb7
Jules <julesbodin04@gmail.com>
2024-12-27 11:50:34 +0100

    Reorganization of the project

--- src/graph.c
+++ src/graph.c
@@ -1,5 +1,11 @@
-#include "graph.h"
+#include "../includes/graph.h"
 
+/**
+ * @brief initialize a new graph of dependencies with empty vertices
+ *
+ * @param nb_vertices the number of vertices
+ * @return the new allocated graph
+ */
 graph_t* init_graph(uint16_t nb_vertices) {
     graph_t* new_graph = (graph_t*)malloc(sizeof(struct graph));
     new_graph->nb_vertices = nb_vertices;
